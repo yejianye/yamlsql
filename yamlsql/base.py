751c093e9f6260ac25d6ec07c7f45411e33f9381
Ryan Ye <yejianye@gmail.com>
2018-05-12 00:07:10 +0800

    Add decimal support for json dump

--- yamlsql/base.py
+++ yamlsql/base.py
@@ -1,9 +1,16 @@
+import decimal
 import json
 import traceback
 
 from flask import request, make_response
 
 from funcy import decorator
+
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, decimal.Decimal):
+            return float(o)
+        return super(DecimalEncoder, self).default(o)
 
 @decorator
 def api_request(func):
@@ -14,13 +21,13 @@ def api_request(func):
         resp_data = json.dumps({
             'status': 'ok',
             'data': func(**params)
-            })
+            }, cls=DecimalEncoder)
     except Exception, e:
         traceback.print_exc()
         resp_data = json.dumps({
             'status': 'error',
             'err_msg': e.message
-            })
+            }, cls=DecimalEncoder)
     response = make_response(resp_data)
     response.headers['Content-Type'] = 'application/json'
     return response
