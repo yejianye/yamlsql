61e0a581ca4ea399b6d88cf01b29747614a79211
Ryan Ye <yejianye@gmail.com>
2018-05-12 00:08:21 +0800

    Emacs package for yamlsql and analytics sql

---
+++ emacs/yamlsql.el
@@ -0,0 +1,157 @@
+;;; -*- lexical-binding: t -*-
+
+(require 'cl)
+(require 'request)
+(setq lexical-binding t)
+
+(defvar yamlsql-conn-id
+  nil
+  "Connection ID to yamlsql server for current buffer.")
+
+(defvar yamlsql-output-buffer
+  "*yamlsql-output*")
+
+(defvar yamlsql--dbmeta-alist
+  '())
+
+(defvar yamlsql--tables-cache
+  nil)
+
+(defconst yamlsql-http-headers
+  '(("Content-Type" . "application/json")
+    ("User-Agent" . "emacs")))
+
+(setq yamlsql-connection-alist
+    '(("localhost"
+          (conn-string . "postgresql://ryan@localhost:5432/yamlsql-test")
+          (search-path . ("public")))))
+
+(setq yamlsql-program "/Users/ryan/source_code/yamlsql/yamlsql/cli.py")
+(setq yamlsql-server-port 15555)
+
+(define-derived-mode yamlsql-mode yaml-mode "YAML-SQL"
+  "Major mode for YAML-SQL format"
+  :abbrev-table nil :syntax-table nil
+  )
+
+(defun yamlsql--output (text)
+  (let ((buffer (get-buffer-create yamlsql-output-buffer)))
+    (with-current-buffer buffer
+      (goto-char (point-max))
+      (insert (format "%s\n\n" text)))
+    (pop-to-buffer buffer))
+  )
+
+(defun yamlsql-start-server ()
+    (start-process-shell-command "yamlsql-server" "*yamlsql-server*"
+        (format "%s runserver --debug --port %d"
+            yamlsql-program yamlsql-server-port))
+    )
+
+(defun yamlsql--http-success (buffer callback)
+  (function* (lambda (&key data &allow-other-keys)
+               (when callback
+                 (with-current-buffer buffer
+                   (apply callback
+                          (list
+                           (assoc-default 'status data)
+                           (assoc-default 'data data))))))
+  ))
+
+(cl-defun yamlsql--http-error (&rest args &key error-thrown &allow-other-keys)
+  (message "yamlsql http error: %S" error-thrown))
+
+(defun yamlsql--http-get (endpoint params &optional callback)
+    (request
+        (format "http://localhost:%s%s" yamlsql-server-port endpoint)
+        :type "GET"
+        :headers yamlsql-http-headers
+        :params params
+        :parser 'json-read
+        :success (yamlsql--http-success (current-buffer) callback)
+        :error 'yamlsql--http-error
+        )
+    )
+
+(defun yamlsql--http-post (endpoint params &optional callback)
+    (request
+        (format "http://localhost:%s%s" yamlsql-server-port endpoint)
+        :type "POST"
+        :headers yamlsql-http-headers
+        :data (json-encode params)
+        :parser 'json-read
+        :success (yamlsql--http-success (current-buffer) callback)
+        :error 'yamlsql--http-error
+        )
+    )
+
+(defun yamlsql--on-connected (status data)
+  (setq-local yamlsql-conn-id (assoc-default 'conn_id data))
+  (message "Status: %S Connection: %S" status yamlsql-conn-id)
+  (yamlsql--build-tables-cache)
+  )
+
+(defun yamlsql-connect (conn-name)
+  (interactive)
+  (let* ((conn-props (assoc-default conn-name yamlsql-connection-alist))
+         (conn-string (assoc-default 'conn-string conn-props))
+         (search-path (assoc-default 'search-path conn-props)))
+    (yamlsql--http-post "/connect"
+                        `(("conn_string" . ,conn-string)
+                          ("search_path" . ,search-path))
+                        'yamlsql--on-connected)
+    (message "Connecting to %s..." conn-name))
+  )
+
+(defun yamlsql--print-text (status data)
+  (yamlsql--output (assoc-default 'text data))
+  )
+
+(defmacro yamlsql--check-conn (&rest body)
+  (append '(if (not yamlsql-conn-id)
+               (message "Please use yamlsql-connect to connect to a DB first."))
+          body)
+  )
+
+(defun yamlsql--build-tables-cache ()
+  (yamlsql--check-conn
+   (yamlsql--http-get "/list_tables"
+                      `(("conn_id" . ,yamlsql-conn-id))
+                      (lambda (status data)
+                        (setq-local yamlsql--tables-cache
+                                    (append (assoc-default 'tables data) nil))
+                        (message "Tables cache has been updated with %d entries."
+                                (length yamlsql--tables-cache)))))
+  )
+
+(defun yamlsql--find-table ()
+  (let ((initial (eldoc-current-symbol))
+        (completion-ignore-case t))
+    (completing-read "Tables :"
+                     yamlsql--tables-cache
+                     nil t initial))
+  )
+
+(defun yamlsql-describe-table (table-name)
+  (interactive (list (yamlsql--find-table)))
+  (yamlsql--check-conn
+   (yamlsql--http-get "/describe_table"
+                      `(("conn_id" . ,yamlsql-conn-id)
+                        ("name" . ,table-name))
+                      'yamlsql--print-text)
+   )
+  )
+
+(defun yamlsql-describe-field (name)
+  (interactive (list (read-from-minibuffer "Field:" (eldoc-current-symbol))))
+  (yamlsql--check-conn
+   (let* ((parts (s-split "\\." name))
+         (field (-last-item parts))
+         (table (s-join "." (-butlast parts))))
+     (yamlsql--http-get "/describe_field"
+                        `(("conn_id" . ,yamlsql-conn-id)
+                          ("table" . ,table)
+                          ("field" . ,field))
+                        'yamlsql--print-text))
+   )
+  )
